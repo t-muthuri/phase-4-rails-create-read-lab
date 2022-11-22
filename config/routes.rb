f28e026e236e847d08df95c308e8ca537356eb99
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-11-22 14:18:53 +0300

    Completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  # get '/plants', to: 'plants#index'
+  # get '/plants/:id', to: 'birds#show'
+  resources :plants, only: [ :index, :show, :create]  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
